23435fc63393d7a3ebf035d30a5c90c195611ae5
kabuto-mk7 <73295078+kabuto-mk7@users.noreply.github.com>
2021-04-15 22:27:06 +0100

    Update netmonitor2.sh

--- netmonitor2.sh
+++ netmonitor2.sh
@@ -8,7 +8,7 @@ for i in $(seq 0 1 $max);
 do
         rec=`ifconfig $ifn | grep RX | tail -2 | head -1 | cut -d' ' -f 11 `
         sent=`ifconfig $ifn | grep TX | tail -2 | head -1 | cut -d' ' -f 11`
-		time=`date+%s`
+		time=`date +%s`
         echo "$rec,$sent,$time" >> output.csv
         sleep 2
         echo "scanning"
